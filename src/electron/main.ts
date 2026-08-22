a02f3d9d2e70c78b1633103804d71a4762bad63c
Ankit <ankitrajdihara123@gmail.com>
2026-08-22 10:18:07 +0530

    hmr_done_for_electron_and_web_app_5173

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,10 +1,15 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./utils.js";
 type test = string;
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  const indexPath = path.join(app.getAppPath(), "dist-react", "index.html");
-   console.log("App path:", app.getAppPath());
-   console.log("Loading:", indexPath);
-  mainWindow.loadFile(indexPath);
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    const indexPath = path.join(app.getAppPath(), "dist-react", "index.html");
+    mainWindow.loadFile(indexPath);
+    console.log("App path:", app.getAppPath());
+    console.log("Loading:", indexPath);
+  }
 });
