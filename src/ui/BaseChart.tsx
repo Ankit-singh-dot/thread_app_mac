627aae30af768d57bb3f6cc1dd94fecdb0bf467c
Ankit <ankitrajdihara123@gmail.com>
2026-09-22 03:54:26 +0530

    grid_show_added

--- src/ui/BaseChart.tsx
+++ src/ui/BaseChart.tsx
@@ -6,32 +6,38 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+
 type BaseChartProps = {
   data: {
     value: number | undefined;
   }[];
+  showGrid?: boolean;
+  fillColor?: string;
+  strokeColor?: string;
 };
 
 export function BaseChart(props: BaseChartProps) {
   return (
     <ResponsiveContainer width={"100%"} height={"100%"}>
       <AreaChart data={props.data}>
-        <CartesianGrid
-          stroke="#333"
-          strokeDasharray=""
-          fill="1C1C1C"
-        ></CartesianGrid>
+        
+
+        {props.showGrid && (
+          <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
+        )}
+
         <Area
-          fillOpacity={0.3}
-          fill={"#0A4D5C"}
-          stroke={"#5DD4EE"}
+          fillOpacity={0.8} // Thoda dark color bhara hai
+          fill={props.fillColor || "#0A4D5C"} // Card se naya color aayega
+          stroke={props.strokeColor || "#5DD4EE"}
           strokeWidth={3}
           type={"monotone"}
           dataKey="value"
           isAnimationActive={false}
         />
-        <XAxis stroke="transparent" height={0} />
-        <YAxis domain={[0, 100]} stroke="transparent" width={0} />
+        
+        {props.showGrid && <XAxis stroke="transparent" height={60} />}
+        {props.showGrid && <YAxis domain={[0, 100]} stroke="transparent" width={0} />}
       </AreaChart>
     </ResponsiveContainer>
   );
