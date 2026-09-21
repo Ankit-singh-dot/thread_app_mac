d448cc0abb88e4290687d351e79fa82c45c7d054
Ankit <ankitrajdihara123@gmail.com>
2026-09-21 11:59:17 +0530

    memos_set_done

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,18 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 import "./App.css";
-import { BaseChart } from "./BaseChart";
+import useStatistics from "./useStatistics";
+import { Chart } from "./Chart";
+// import { BaseChart } from "./BaseChart";
 
 function App() {
-  useEffect(() => {
-    const unsubscribe = window.electron?.subscribeStatics?.((stats) => {
-      console.log(stats);
-    });
-    return () => {
-      unsubscribe?.();
-    };
-  }, []);
-
+  const statistics = useStatistics(10);
+  console.log(statistics);
+  const cpuUsages = useMemo(
+    () => statistics.map((stat) => stat.cpuUsage),
+    [statistics]
+  );
   useEffect(() => {
     async function fetchSpecs() {
       const specs = await window.electron?.getStaticsData();
@@ -23,12 +22,9 @@ function App() {
 
   return (
     <div className="app">
-      <div style={{ height: 400 }}>
-        <BaseChart
-          data={[{ value: 10 }, { value: 30 }, { value: 60 }, { value: 100 }]}
-        ></BaseChart>
+      <div style={{ height: 120 }}>
+        <Chart data={cpuUsages} maxPointData={10} />
       </div>
-
       <div className="naming"> hello</div>
     </div>
   );
